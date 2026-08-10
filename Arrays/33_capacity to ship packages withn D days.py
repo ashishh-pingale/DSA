e7ea03f55e9dbbb20402d104e2f03977e369d179
Your Name <ashishpingale22@gmail.com>
2026-08-10 22:05:53 +0530

    solved the capacity to ship packages within D days

---
+++ Arrays/33_capacity to ship packages withn D days.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def shipWithinDays(self, weights, days):
+        left = max(weights)
+        right = sum(weights)
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            days_used = 1
+            current_weight = 0
+
+            for weight in weights:
+                if current_weight + weight > mid:
+                    days_used += 1
+                    current_weight = weight
+                else:
+                    current_weight += weight
+
+            if days_used <= days:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
