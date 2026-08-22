39be84510e16a249bf66f83db14ec574cf801407
Your Name <ashishpingale22@gmail.com>
2026-08-22 15:36:07 +0530

    Solved the Contain Duplicate problem

---
+++ Arrays/41_contains duplicate.py
@@ -0,0 +1,12 @@
+def dup(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen .add(num)
+    return False
+
+nums = [1,2,3,1]
+
+print(dup(nums))
