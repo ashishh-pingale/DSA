6253e46b753f762b635ad9ace39f92b25819f305
Your Name <ashishpingale22@gmail.com>
2026-08-23 11:24:43 +0530

    Solved the longest consecutive sequence problem

---
+++ Arrays/42_Longest consecutive sequence.py
@@ -0,0 +1,18 @@
+def longest(nums):
+    num_set = set(nums)
+    longest = 0
+
+    for num in num_set:
+        if num - 1 not in num_set:
+            current = 1
+            length = 1
+
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+
+            longest = max(longest , length)
+    return longest
+
+nums = [100,4,200,1,3,2]
+print(longest(nums))
