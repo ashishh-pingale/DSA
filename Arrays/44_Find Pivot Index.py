d1b11c5ef035b28d4f0922befbf2cd4adaea486d
Your Name <ashishpingale22@gmail.com>
2026-08-26 20:11:35 +0530

    Solved the Find Pivot Index Problem

---
+++ Arrays/44_Find Pivot Index.py
@@ -0,0 +1,16 @@
+def pivot(nums):
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i in range(len(nums)):
+        right_sum = total_sum - left_sum - nums[i]
+
+        if right_sum ==  left_sum:
+            return i
+
+        left_sum += nums[i]
+    return -1
+
+nums = [1,7,3,6,5,6]
+print(pivot(nums))
+
