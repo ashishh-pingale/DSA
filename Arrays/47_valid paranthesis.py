7f404da793627d264600342f8b2672317cfac2ff
Your Name <ashishpingale22@gmail.com>
2026-08-28 18:56:20 +0530

    Solved the Valid paranthesis Problem Using Stack

---
+++ Arrays/47_valid paranthesis.py
@@ -0,0 +1,22 @@
+def isValid(s):
+    stack = []
+
+    pairs = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+
+    for char in s:
+        if char in "([{":
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            if stack[-1] != pairs[char]:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
+s = "([])"
+print(isValid(s))
